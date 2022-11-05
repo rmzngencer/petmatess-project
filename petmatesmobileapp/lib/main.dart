6cfddbc8cc2398ec96a7d804af4e21fa84d4dabc
utkukozan <57047195+utkukozan@users.noreply.github.com>
2022-11-05 14:48:49 +0300

    Update main.dart

--- petmatesmobileapp/lib/main.dart
+++ petmatesmobileapp/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',ssaas
+      title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
         //
