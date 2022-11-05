fd82365be36ceb12cf921606c5bbde89b244d17d
goksuturac <goksuturac@eskisehir.edu.tr>
2022-11-05 18:07:03 +0300

    4

--- petmatesmobileapp/lib/views/LoginPage.dart
+++ petmatesmobileapp/lib/views/LoginPage.dart
@@ -1,26 +1,30 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'dart:ffi';
 
-class LoginPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:petmatesmobileapp/constants/ReusableWidgets.dart';
+
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
   Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Column(
-          children: [
-            Container(
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage("assets/images/Pattern.png"),
-                ),
-              ),
-            )
-          ],
+      appBar: ReusableWidgets.getAppBar(),
+       body: Container(
+          height: height,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/Pattern.png"),
+              fit: BoxFit.cover,
+            ),
+          ),
         ),
-      ),
-    );
+      );
   }
 }
